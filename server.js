e24dbb5ad7fd21ef25cffe8b6c10582d1022a2fc
Tobin Walton <tobin.walton@gmail.com>
2016-05-12 10:49:52 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express    = require('express');
+var app        = express();
+
+// ToDo:
+// 1. After receiving date string, parse date string.
+// 2. Determine if date string contains 'natural language' date or Unix date.
+// 3. If a valid date is passed in 'natural' or Unix format, translate it to the other format.
+// 4. Make the date pretty and readable in both formats.
+// 5. Return the date in both formats.
+
+// Function to parse date. Use regex to detect groupings of numbers/ letters in date string?
+var parseDate = function(date) {
+	return date;
+};
+
+// string passed at '/' is saved to date variable and returned.
+app.get('/:date', function(req, res) {
+
+	var date = req.params.date;
+	res.send('date: ' + date);
+});
+
+app.listen(8080, function(con));
